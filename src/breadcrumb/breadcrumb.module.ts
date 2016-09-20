51b93dd08e4a61989991d638f8fb0e208db16ced
Philipp Andreas <philipp.andreas@vanzoo.de>
2016-09-20 17:00:12 +0200

    Started angular 2.0.0 support

--- src/breadcrumb/breadcrumb.module.ts
+++ src/breadcrumb/breadcrumb.module.ts
@@ -1,2 +1,16 @@
-export declare class BreadcrumbModule {
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { BreadcrumbComponent } from './breadcrumb.component';
+import { BreadcrumbService } from './breadcrumb.service';
+
+@NgModule({
+    imports: [CommonModule, FormsModule, RouterModule],
+    declarations: [BreadcrumbComponent],
+    providers: [BreadcrumbService],
+    exports: [BreadcrumbComponent]
+})
+export class BreadcrumbModule {
 }
